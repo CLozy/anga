915def10f6b2fa3c8ed9642cc4476e13874cb9c3
Clozy <clozymwangs@gmail.com>
2022-06-10 11:15:54 +0300

    scraping weather data from google

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,52 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import requests
+from bs4 import BeautifulSoup
+
+class ActionWeatherForecast(Action):
+
+    def name(self) -> Text:
+        return "action_weather_forecast"
+
+
+    def weather_data(self, city):
+
+        # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        #create url 
+        # res = requests.get(f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',headers=headers)
+        
+        params ={
+            "q" : f"weather{city}",
+            "hl" : "en"
+        }
+        url = "https://www.google.com/search"
+       
+
+        #requests instance
+        html = requests.get(url, params=params).content
+
+        # getting raw data
+        soup = BeautifulSoup(html, "html.parser")
+        return soup
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+weather_forecast = ActionWeatherForecast()
+data = weather_forecast.weather_data("london")
+print(data)
+
